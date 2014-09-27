2baa56f1ef1a932af55f9bb2c692c5d390528d72
unzan <mystia_lorelei@yahoo.com>
2014-09-27 17:13:35 +0700

    update build script
    force no sourcemap by default. pass env var SOURCEMAP when calling the
    script if you want to have sourcemap, eg:
    
      SOURCEMAP=file ./build.sh -w

--- build.sh
+++ build.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
 
-if [ "$1" = "-w" -o "$1" = "--watch" ]; then
-   PARAM="--watch scss:."
-else
-   PARAM="scss/terriblename.scss terriblename.css"
+# $1 = actual version
+# $2 = min version
+function check_min_version {
+   if [ "$1" = "$2" ]; then
+      echo "$1"
+   else
+      local v=$(echo -e "$1\n$2" | sort -rV | head -1)
+      if [ "$v" = "$1" ]; then
+         echo "$1"
+      fi
+   fi
+}
+
+SOURCEMAP="${SOURCEMAP:-none}"
+PARAM="--unix-newlines --style=expanded"
+VERSION=$(gem list sass | sed -r '/^sass /!d;s/^[^(]+\(([^)]+)\)$/\1/')
+
+if [ -z "$VERSION" ]; then
+   echo "This script requires the following ruby gem: sass" >&2
+   exit 1
 fi
 
-scss --style=expanded $PARAM
+if [ "$(check_min_version "$VERSION" 3.3.0)" = "$VERSION" ]; then
+   PARAM="${PARAM} --sourcemap=$SOURCEMAP"
+fi
+
+if [ "$1" = "-w" -o "$1" = "--watch" ]; then
+   PARAM="${PARAM} --watch"
+else
+   PARAM="${PARAM} --update"
+fi
+
+scss $PARAM scss:.
